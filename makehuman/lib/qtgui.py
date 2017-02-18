d969a622e4624b025b0e43cd0a0c7529772decea
rwbaer <rwbaer@gmail.com>
2017-02-18 13:40:51 -0600

    fix bad return blocking startup screen

--- makehuman/lib/qtgui.py
+++ makehuman/lib/qtgui.py
@@ -644,7 +644,7 @@ class ListItem(QtGui.QListWidgetItem):
         self.setData(QtCore.Qt.UserRole, data)
 
     def getUserData(self):
-        return self.data(QtCore.Qt.UserRole).toPyObject()
+        return self.data(QtCore.Qt.UserRole)
 
     def setText(self, text):
         super(ListItem, self).setText(text)
